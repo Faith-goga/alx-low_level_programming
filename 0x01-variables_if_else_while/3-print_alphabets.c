60642cfb2f632a3b024d37c6b8e6ca89ef10d0c7
Faith-goga <83953243+Faith-goga@users.noreply.github.com>
2021-08-27 20:32:03 +0300

    Create 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,13 @@
 
 int main(void)
 {
-   char low;
-   for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+	char low;
 
-  return (0);
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
 }

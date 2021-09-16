e3a09d2a2c3559ec7941fba977949fa5d3ddabc9
Faith-goga <faithgoga@gmail.com>
2021-09-16 20:46:07 +0300

    Correct answer in file 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,18 +1,35 @@
 #include "main.h"
 
 /**
- *  is_palindrome - find a palindrome string
- * @s: string to check
+ *  is_palindrome_helper - helper function for finding a palindrome
+ * @s: int, counter to start
+ * @e: int, counter to end
+ * @c: char, the string
+ * Return: returns int boolean if palindrome or not
+ */
+int is_palindrome_helper(char *c, int s, int e)
+{
+	if (s == e)
+		return (1);
+	if (c[s] != c[e])
+		return (0);
+	if (s < e + 1)
+		return (is_palindrome_helper(c, s + 1, e - 1));
+	return (1);
+}
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: pointer to char, an array
  *
- * Return:  1 if is palindrome, otherwise 0
+ * Return: returns in 1 if palindrome 0 if not
  */
 int is_palindrome(char *s)
 {
-	int i;
+	int count = getLen(s);
 
-	i = 0;
-
-	if (*s == i)
-		return (0);
-	return (1);
+	if (count == 0)
+		return (1);
+	return (is_palindrome_helper(s, 0, count - 1));
 }
+

05ad810eacb85cd2a3a6919cc49cf37753afbbed
Faith-goga <83953243+Faith-goga@users.noreply.github.com>
2021-09-23 22:22:09 +0300

    Correct answer for file 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,52 @@
 #include "main.h"
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: destination string
- * @s2: source string
- * @n: number of bytes from s2 to be copied
- * Return: concatenated string
- */
+*string_nconcat -  function that concatenates two strings.
+*
+*@s1: string one
+*@s2: string two
+*@n: number of byte to copy
+*
+*Return: concatenation of two strings
+*/
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, size, len1, len2;
-	char *ptr;
+	char *concat_str;
+	unsigned int s1_size = 0, s2_size = 0, counter = 0, counter2 = 0;
 
 	if (s1 == NULL)
-		s1 = "";
+	s1 = "";
+
 	if (s2 == NULL)
-		s2 = "";
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	if (n > len2)
-		n = len2;
-	size = len1 + n;
-	ptr = malloc(sizeof(char) * size + 1);
-	if (!ptr)
+	s2 = "";
+
+
+	while (*(s1 + s1_size) != '\0')
+	s1_size++;
+
+	while (*(s2 + s2_size) != '\0')
+	s2_size++;
+
+	if (n < s2_size)
+		s2_size = n;
+
+	concat_str = malloc((s1_size + s2_size + 1) * sizeof(char));
+
+	if (concat_str == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		ptr[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
-		ptr[i] = s2[j];
-	ptr[i] = 0;
-	return (ptr);
-}
 
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	char *p = s;
+	for (counter = 0; counter < s1_size; counter++)
+	{
+	concat_str[counter] = s1[counter];
+	}
 
-	while (*s)
-		s++;
-	return (s - p);
+	for (counter2 = 0; counter2 < s2_size; counter++, counter2++)
+	{
+	concat_str[counter] = s2[counter2];
+	}
+
+	concat_str[counter] = '\0';
+
+	return (concat_str);
 }
